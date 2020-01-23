dfc1fd522d56954344f6c798dec5ac768ed12f2f
Dusan Srbulovic <dsrbulovic@Dusans-MacBook-Pro.local>
2020-01-23 16:30:49 +0100

    Odradjena paginacija za movie list

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,13 +1,20 @@
 import ApiService from './ApiService';
 
 const ENDPOINTS = {
-  MOVIES: '/api/movies'
+  MOVIES: '/api/movies',
+  MOVIES_COUNT: '/api/count/movies',
 };
 
 class MovieService extends ApiService {
   getMovies = () => {
     return this.apiClient.get(ENDPOINTS.MOVIES);
   };
+  getMoviesByPage = payload => {
+    return this.apiClient.get(ENDPOINTS.MOVIES + "?offset=" + payload.offset +"&take=" + payload.take);
+  }
+  getMoviesCount = () =>{
+    return this.apiClient.get(ENDPOINTS.MOVIES_COUNT);
+  }
 }
 
 export const movieService = new MovieService();
--- src/store/actions/MovieActions.js
+++ src/store/actions/MovieActions.js
@@ -1,5 +1,10 @@
-import { GET_MOVIES, SET_MOVIES } from './ActionTypes';
-
+import { GET_MOVIES, SET_MOVIES, GET_MOVIES_BY_PAGE, SET_MOVIES_COUNT, GET_MOVIES_COUNT} from './ActionTypes';
+export const getMoviesByPage = payload =>{
+  return {
+    type: GET_MOVIES_BY_PAGE,
+    payload
+  }
+}
 export const getMovies = () => {
   return {
     type: GET_MOVIES
@@ -9,6 +14,17 @@ export const getMovies = () => {
 export const setMovies = payload => {
   return {
     type: SET_MOVIES,
+    payload
+  };
+};
+export const getMoviesCount = () =>{
+  return {
+    type : GET_MOVIES_COUNT
+  }
+}
+export const setMoviesCount = (payload) =>{
+  return {
+    type: SET_MOVIES_COUNT,
     payload
   };
 };
