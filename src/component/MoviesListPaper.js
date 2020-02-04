0c7919bb6b6d93492879ef7741b7cae902e28533
Dusan Srbulovic <dsrbulovic@Dusans-MacBook-Pro.local>
2020-02-04 09:59:09 +0100

    ispravka

--- src/component/MoviesListPaper.js
+++ src/component/MoviesListPaper.js
@@ -8,23 +8,25 @@ import Checkbox from '@material-ui/core/Checkbox';
 import { fetchPopularMovies, setSelectedMovie } from '../store/actions/MovieActions';
 import { Link } from 'react-router-dom';
 class MoviesListPaper extends React.Component{
-    
-    
     setSelectedMovie =(movie)=>{
         this.props.setSelectedMovie(movie);
-    }
-    renderPopularMovies = () =>{
-        return this.props.movies.map(movie => {
-            return (<div key={movie.id} style={{textAlign:'lefgt'}}><Link onClick={()=>this.setSelectedMovie(movie)} style={{width:'100%', textAlign:'left', marginLeft:'5%'}}  to={"/movie/" + movie.id}> {movie.title}</Link><br></br></div>)
-        })
-
-    }
+    }  
     render() {
         return (
         <Paper style={{marginLeft:'20px', marginTop:'5%'}}>
             {this.props.title}
             <br/>
-            {this.renderPopularMovies()}
+            {this.props.movies.map(movie => {
+                return (
+                    <div key={movie.id} style={{textAlign:'lefgt'}}>
+                        <Link 
+                            onClick={()=>this.setSelectedMovie(movie)} 
+                            style={{width:'100%', textAlign:'left', marginLeft:'5%'}}  
+                            to={"/movie/" + movie.id}> {movie.title}
+                        </Link>
+                        <br></br>
+                    </div>)
+            })}
             </Paper>
         );
     }
